681f8993386e947a4263cc40911eaaf3a5c6233f
TJ Holowaychuk <tj@vision-media.ca>
2016-04-05 09:39:35 -0700

    fix example format function

--- example/index.js
+++ example/index.js
@@ -21,7 +21,7 @@ const gen = n => {
 class App extends Component {
   componentDidMount() {
     const tip = new Tip({
-      format: d3.format(',')
+      format: d => d3.format(',')(d.value)
     })
 
     this.a = new BarChart({
